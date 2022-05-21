9c988561b11e324bd9cad0bb690594f939e0fa6c
Taychinov Evgeny <helloevgenyy@gmail.com>
2022-05-22 01:47:55 +0500

    Outliers were removed

--- preprocessing.py
+++ preprocessing.py
@@ -21,21 +21,35 @@ class DataPreprocessingPipeline:
         return X
 
 def convert_data(df):
+    # Remove outliers (less than 10 vmail messages and more than 50)
+    mask_vm_true = (df.voice_mail_plan == 'yes')
+    mask_vm_outlier = ((df.number_vmail_messages > 50) | (df.number_vmail_messages < 10))
+    df = df.drop(df[mask_vm_true & mask_vm_outlier].index)
+
+    # More or equal to 5 service calls for non-churn customers
+    mask_ch_true = (df.churn == 'no')
+    mask_ch_outlier = (df.number_customer_service_calls >= 5)
+    df = df.drop(df[mask_ch_true & mask_ch_outlier].index)
+
+    # Map categorical columns to numbers
     bin_columns = ['international_plan', 'voice_mail_plan', 'churn']
     for col in bin_columns:
         df[col] = df[col].map({'yes': 1, 'no': 0})
 
+    # Sum up total columns, instead of different times of a day
     feature_types = ['minutes', 'calls', 'charge']
 
     for feature in feature_types:
         df['total_' + feature] = df['total_day_' + feature] + df['total_eve_' + feature] + df['total_night_' + feature]
         df.drop(['total_day_' + feature, 'total_eve_' + feature, 'total_night_' + feature], axis=1, inplace=True)
 
+    # Find Top 5 states by churn
     top5_states = df[df['churn'] == 1]['state'] \
         .value_counts() \
         .sort_values(ascending=False)[:5] \
         .index.values
 
+    # Keep Top 5 and label others as Other
     df['state'] = df['state'].apply(lambda x: x if x in top5_states else 'other')
     df = pd.get_dummies(df, columns=['area_code', 'state'])
 
